72b1acd58f27ed5a11ba1180e675cbd44487f1ad
kwon <kyjyj6334@gmail.com>
2023-10-24 23:01:23 +0900

    entity 어노테이션 autoincreament제거

--- src/main/java/com/example/bootjpa/data/entity/ProductEntity.java
+++ src/main/java/com/example/bootjpa/data/entity/ProductEntity.java
@@ -23,7 +23,6 @@ public class ProductEntity {
     //entity란 데이터베이스의 테이블과 직접적으로 매핑이되는 클래스를 뜻함.
 
     @Id //어노테이션 Id ㅜ
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 식별자 값이 자동으로 생성되도록 설정
     String productId; //primaryKey
 
     String productName;
